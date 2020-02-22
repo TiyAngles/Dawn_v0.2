602a8f8a5dfcfb508658d1a89256ff2a4d102ddf
sohyun0 <thgus2532@naver.com>
2020-02-22 19:32:41 +0900

    sub_season.html

---
+++ Publishing/html/js/swiper_sub.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 1, // 동시에 보여줄 슬라이드 갯수
+    spaceBetween: 0, // 슬라이드간 간격
+    centeredSlides: true //카드슬라이드옵션
+  });
+});

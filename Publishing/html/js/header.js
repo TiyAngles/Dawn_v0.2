6c88a1cd9cc409fb1b71a9e774f790debf153289
unknown <kwakms821@daum.net>
2020-02-22 18:40:13 +0900

    change js

--- Publishing/html/js/header.js
+++ Publishing/html/js/header.js
@@ -8,6 +8,14 @@ $(document).ready(function() {
         $('nav').stop().animate({"left":"100%"});
     });
 
+    $('.gnb-list').on('click',function(){
+        $(this).children('.open-menu').slideToggle();
+    });
+    $('.open-menu>li').on('click',function(){
+        $(this).addClass('on');
+        $(this).siblings().removeClass('on');
+    });
+
     $('#searchBox').on('click',function(){
         $('#searchInput').toggleClass('on');
     })

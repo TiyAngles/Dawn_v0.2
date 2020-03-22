6f0074b80ee3c0098962a63c47e8216ff7b04d87
sohyun0 <thgus2532@naver.com>
2020-03-23 00:10:35 +0900

    로그인관련 추가페이지

--- Publishing/html/js/login_find_tab.js
+++ Publishing/html/js/login_find_tab.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
     $(".find-tab ul li").eq(0).click(function () {
+        $(this).find("a").addClass("on");
+        $(".find-tab ul li").eq(1).find("a").removeClass("on");
         $(".forget-id").show();
         $(".forget-pw").removeClass("on");
     });
     $(".find-tab ul li").eq(1).click(function () {
+        $(this).find("a").addClass("on");
+        $(".find-tab ul li").eq(0).find("a").removeClass("on");
         $(".forget-pw").addClass("on");
         $(".forget-id").hide();
     });

9d19b907fcaac4b37a918f93429ae7dbfa3c2160
yeeun park <charm5231@naver.com>
2020-03-28 16:18:03 +0900

    update user account, login, add user edit

--- Back/src/api/User/Login/Login.js
+++ Back/src/api/User/Login/Login.js
@@ -1,19 +1,18 @@
 export default {
     Mutation: {
       Login: () => async (_, args) => {
-        const { id, password } = args;
-        const user = prisma.user({id});
+        const { email, password } = args;
+        const user = prisma.user({email});
         if(!user) {
             throw Error("사용자가 존재하지 않습니다.");
         }
 
         // 나중에 암호화 방식 확인 후 변경 필요?
-        const checkPwd = await bcrypt.compare(password, usr.password);
-        if(!checkPwd) {
-            throw new Error("비밀번호가 일치하지 않습니다.");
+        const checkPwd = await bcrypt.compare(password, user.password);
+        
+        if(user.password.compare === password) {
+            //throw new Error("비밀번호가 일치하지 않습니다.");
         }
-
-        return true;
       }
     }
   };

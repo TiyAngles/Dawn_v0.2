7fafa34b4ecb30885ae8410726921415a5e175b7
vapvin <tpehdj12@daum.net>
2020-04-05 16:09:05 +0900

    Slide Handler

--- Publishing/new_html/js/slide.js
+++ Publishing/new_html/js/slide.js
@@ -22,34 +22,150 @@ slidSelect()
 
 // Slide Object
 
-class Swipes {
-    constructor(item, dur, chi){
-        this.item = item;
-        this.dur = dur;
-        this.chi = chi;
-    }
+// class Swipes {
+//     constructor(item, dur, chi){
+//         this.item = item;
+//         this.dur = dur;
+//         this.chi = chi;
+//     }
 
-    setItem(item, chi){
+//     setItem(item, chi){
         
-        const widths = chi[0].offsetWidth;
-    }
+//         const widths = chi[0].offsetWidth;
+//     }
 
-    sliding(item, dur){
-       setItem();
-       let pos = this.widths;
-       if(){
-        item.style.left = -pos;
-       }else if(){
-        item.style.left = pos;
-       }
-    }
-    item.addEventListener("mousedown",setItem);
-    item.addEventListener("mousemove", sliding);
-    item.addEventListener("mousedrop", function(){
-        return false;
-    });
-}
+//     sliding(item, dur){
+//        setItem();
+//        let pos = this.widths;
+//        if(){
+//         item.style.left = -pos;
+//        }else if(){
+//         item.style.left = pos;
+//        }
+//     }
+//     item.addEventListener("mousedown",setItem);
+//     item.addEventListener("mousemove", sliding);
+//     item.addEventListener("mousedrop", function(){
+//         return false;
+//     });
+// }
 
-var test = new Swipes(slide_wrap, 500, con_slide);
+// var test = new Swipes(slide_wrap, 500, con_slide);
 // Event -> Search
 // mousedown d는 이거라서 
+
+// --------------------------------------------------------- //
+
+const slider = document.getElementsByClassName()[0];
+const info = document.getElementsByClassName()[0];
+
+let canMove = false;
+let touchDown = 0;
+let prevX = 0;
+let slides = document.getElementsByClassName('c-slide');
+const slideWidth = slides[0].offsetWidth + 20;
+
+
+const init = () => {
+  slider.addEventListener('mousedown', handleMouse);
+  slider.addEventListener('mouseup', handleMouse);
+  slider.addEventListener('mousemove', handleMove);
+  
+  slider.addEventListener('touchstart', handleTouch);
+  slider.addEventListener('touchmove', handleTouchMove);
+};
+
+
+const handleMouse = e => {
+  if (e.type === 'mouseup') {
+    canMove = false;
+  } else {
+    canMove = true;
+  }
+};
+
+const handleMove = e => {
+  if (e.pageX < prevX && canMove) {
+
+    info.innerHTML = 'Swiping left!';
+    handleSwipeLeft();
+    canMove = false;
+  } else if (e.pageX > prevX && canMove) {
+    
+    info.innerHTML = 'Swiping right!';
+    handleSwipeRight();
+    canMove = false;
+  }
+
+  prevX = e.pageX;
+};
+
+
+const handleTouch = e => {
+  touchDown = e.touches[0].clientX
+}
+
+const handleTouchMove = e => {
+  if (!touchDown) {
+    return
+  }
+
+  const touchUp = e.touches[0].clientX
+  const touchDiff = touchDown - touchUp
+
+  if (touchDiff > 0) {
+    info.innerHTML = 'Swiping left!';
+    handleSwipeLeft();
+  } else {
+    info.innerHTML = 'Swiping right!';
+    handleSwipeRight();
+  }
+
+   touchDown = null
+ };
+
+
+const handleSwipeLeft = () => {  
+  slideses.fromTo(slider, 1, 
+    {
+      x: '0px'
+    },
+    {
+      x: `-${slideWidth}px`,
+      ease: Power4.easeOut
+    }
+  );
+  
+  slideses.to(slider, 0.001, 
+    {
+      x: '0px',
+      onComplete: () => {
+        slider.appendChild(slides[0]);
+        slides = document.getElementsByClassName('c-slide');
+      }
+    }
+  );
+};
+
+const handleSwipeRight = () => {  
+  slideses.to(slider, 0.001, 
+    {
+      x: `-${slideWidth}px`,
+      onComplete: () => {
+        const first = slides[0];
+        const last = slides[slides.length - 1];
+        slider.insertBefore(last, first);
+        slides = document.getElementsByClassName('c-slide');
+      }
+    }
+  );
+  
+  slideses.to(slider, 1, 
+    {
+      x: `0px`,
+      ease: Power4.easeOut
+    }
+  );
+};
+
+init();
